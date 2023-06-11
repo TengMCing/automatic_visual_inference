7e8df5314b60c44d8bb93800b4fa2777514672f5
TengMCing <llreczx@gmail.com>
2023-06-12 08:10:40 +1000

    update git ignore

--- tensorboard_startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh  
-
-# This only works for homebrew miniconda TF
-# Replace it with your own TensorBoard path
-# Or you could try `tensorflow::tensorboard(here::here("logs"))` which may or
-# may not work for your machine.
-/opt/homebrew/Caskroom/miniconda/base/envs/tensorflow/bin/tensorboard --logdir logs                   
-                   
-
